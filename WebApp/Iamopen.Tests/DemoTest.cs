8936e91a88c7863974a4137cfcfec1384987cf06
moimusique <mr.matsekh@gmail.com>
2011-10-23 20:34:16 +0300

    Adding nUnit test project

---
+++ WebApp/Iamopen.Tests/DemoTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Iamopen.Tests
+{
+    [TestFixture]
+    public class DemoTestClass
+    {
+        [Test]
+        public void DemoTest()
+        {
+            // Arrange
+            int data = 5;
+
+            // Act
+            int actualResult = data + 2;
+                          
+            // Assert
+            int expectedResult = 7;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
