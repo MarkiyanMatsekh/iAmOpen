f19fbe28f203a0221efc86eb959791352e4d5b98
moimusique <mr.matsekh@gmail.com>
2011-10-24 09:30:17 +0300

    Making more depp level of abstraction; reusing some code

---
+++ WebApp/Iamopen.Model/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using IAmOpen.Model.Models;
+
+namespace IAmOpen.Model.Abstractions
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        IGenericRepository<Institution> InstitutionRepository { get; }
+
+        IGenericRepository<InstitutionType> InstitutionTypeRepository { get; }
+
+        IGenericRepository<User> UserRepository{ get; }
+
+        IGenericRepository<Chain> ChainRepository{ get; }
+
+        IGenericRepository<ExternalAccount> ExternalAccountRepository{ get; }
+
+        IGenericRepository<WorkTime> WorkTimeRepository{ get; }
+
+        IGenericRepository<Vote> VoteRepository{ get; }
+
+        IGenericRepository<Review> ReviewRepository{ get; }
+
+        IGenericRepository<UserExternalAccountToken> UserExtAccountRepository{ get; }
+
+        IGenericRepository<Status> StatusRepository{ get; }
+
+        IGenericRepository<Role> RoleRepository{ get; }
+
+        IGenericRepository<State> StateRepository{ get; }
+
+        void Save();
+    }
+}
