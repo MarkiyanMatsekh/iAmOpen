ffea18b3d10ca430e639a77526214f419605b4e9
moimusique <mr.matsekh@gmail.com>
2011-10-28 00:04:55 +0300

    Adding comments on javascript examples

--- Study/Javascript/BohdanP/main.js
+++ Study/Javascript/BohdanP/main.js
@@ -1,3 +1,7 @@
+// in general - very good, especially as for javascript-beginner. The most of notes are minor. The main tasks are made greatly
+
+(function() { // we don't let the garbage outside =)
+
 window.onload = function(){	
 	//alert("37: " + isPrime(37));
 	addBubbleSortToArray();
@@ -24,10 +28,10 @@ window.onload = function(){
 
 //Sort function
 function addBubbleSortToArray(){
-	Array.prototype.bubbleSort = function (isBigger){
+	Array.prototype.bubbleSort = function (isBigger){ // does this delegate really tells who's Bigger? it seems like he only _compares_ the values, without specific knowledge of them
 		for (var i = this.length; i > 0; i--){
 			for (var j = 0; j < i - 1; j++){
-				if (isBigger(this[j], this[j+1])){
+				if (isBigger(this[j], this[j+1])){ 
 					var c = this[j];
 					this[j] = this[j+1];
 					this[j + 1] = c;
@@ -56,14 +60,14 @@ function Car(carModel, carYear){
 
 function overrideCarToString(){
 	Car.prototype.toString = function(){
-		return this.model + ", " + this.year + "\n";
+		return this.model + ", " + this.year + "\n";// adding the newline at the end of textual representation of a car doesn't sound like a good idea. To achieve showing array on different lines use [].join('\n');
 	}
 };
 
 function useCar(){
-	overrideCarToString();
+	overrideCarToString(); // better do this right after 'Car' definition, if you want all Cars to behave the same way
 
-	var carArr = new Array();
+	var carArr = new Array(); // === var carArr = [new Car("Audi A4", 2001), new Car("Audi A7", 2010),new Car("Porshe 911", 2001) ...];
 	carArr[0] = new Car("Audi A4", 2001);
 	carArr[1] = new Car("Audi A7", 2010);
 	carArr[2] = new Car("Porshe 911", 2001);
@@ -83,7 +87,7 @@ function Circle(radius, color){
 	var _radius = radius;
 	var _color = color;
 	
-	this.getRadius = function(){ return _radius;}
+	this.getRadius = function(){ return _radius;} // it's not C#, better put an extra semilicon on each line=)
 	this.setRadius = function(radius){_radius = radius;}
 	this.getColor = function(){ return _color;}
 	this.setColor = function(color){_color = color;}
@@ -103,3 +107,5 @@ function useCircle(){
 	alert(circ);
 	alert(circ.getRadius());
 }
+
+})();
