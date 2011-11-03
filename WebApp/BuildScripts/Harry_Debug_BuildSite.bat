41f6e469d110e3ff839a00fa42a1fc7189a3781d
moimusique <mr.matsekh@gmail.com>
2011-11-03 11:21:27 +0200

    adding explanation of using build script

--- WebApp/BuildScripts/Harry_Debug_BuildSite.bat
+++ WebApp/BuildScripts/Harry_Debug_BuildSite.bat
@@ -1,1 +1,3 @@
 _buildSite Debug D:\Stuff\work\iAmOpen\WebApp D:\temp\iamopen
+rem ---how to use
+rem _buildSite (build configuration) (path to project on your machine) (path to deploy the project(usually - path that IIS uses to host your site))
