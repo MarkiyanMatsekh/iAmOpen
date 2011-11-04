cecedbcffc898a345fe5f77dce3f0c5589bbc777
moimusique <mr.matsekh@gmail.com>
2011-11-05 00:31:37 +0200

    1.adding ajax mode;
    2.adding default controller

--- WebApp/Iamopen/Global.asax.cs
+++ WebApp/Iamopen/Global.asax.cs
@@ -24,7 +24,7 @@ namespace Iamopen
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Institution", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
